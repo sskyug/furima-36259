842a368a26f740aae53799e9321f2502f9692da6
sskyug <sskyugo972@gmail.com>
2022-02-02 10:46:20 +0900

    usersテーブル作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,22 @@
 class ApplicationController < ActionController::Base
-before_action :basic_auth
+  before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  private
 
-private
-
-def basic_auth
-  authenticate_or_request_with_http_basic do |username, password|
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
     username == 'admin' && password == '2222'
+    end
   end
-end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name_kana])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name_kana])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:birth_day])
+  end
 
 end
