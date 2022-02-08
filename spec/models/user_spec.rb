8d43ab2464a0a6b71b83a6f48730f1eef81dbec5
sskyug <sskyugo972@gmail.com>
2022-02-08 14:40:08 +0900

    テストコード修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -39,6 +39,11 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Password can't be blank")
     end
+    it '生年月日が空では登録できない' do
+      @user.birth_day = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birth day can't be blank")
+    end
     it 'passwordとpassword_confirmationが不一致では登録できない' do
       @user.password = '123456'
         @user.password_confirmation = '1234567'
@@ -66,6 +71,16 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
+    it '姓（全角）が空では登録できない' do
+      @user.last_name = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name can't be blank")
+    end
+    it '名（全角）が空では登録できない' do
+      @user.first_name = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name can't be blank")
+    end
     it '姓（全角）に半角文字が含まれていると登録できない' do
       @user.last_name = "test"
       @user.valid?
@@ -75,6 +90,16 @@ RSpec.describe User, type: :model do
       @user.first_name = "test"
       @user.valid?
       expect(@user.errors.full_messages).to include("First name is invalid")
+    end
+    it '姓（カナ）が空では登録できない' do
+      @user.last_name_kana = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name kana can't be blank")
+    end
+    it '名（カナ）が空では登録できない' do
+      @user.first_name_kana = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name kana can't be blank")
     end
     it '姓（カナ）にカタカナ以外の文字が含まれていると登録できない' do
       @user.last_name_kana = "test"
