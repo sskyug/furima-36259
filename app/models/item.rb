444c9729a1de145c054c051d666791f129265cd9
sskyug <sskyugo972@gmail.com>
2022-03-05 12:32:04 +0900

    itemモデルにバリデーションを記載

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,15 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :send_day_id
+
   belongs_to :user
-  
+
+  validates :items_name, :detail, :category_id, :condition_id, :delivery_charge_id, 
+  :prefecture_id, :send_day_id, :price, presence: true
+
+  validates :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :send_day_id,
+  numericality: { other_than: 1 , message: "can't be blank"}
+
   has_one_attached :image
 
 end
