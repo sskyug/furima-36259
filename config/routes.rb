d810dfb59b70ba29a2557163bbcf4050c294563c
sskyug <sskyugo972@gmail.com>
2022-03-05 18:44:30 +0900

    ルーティングの記載ミスの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-  root to: "items#index"
-  resources :items
-  get 'items/index'
+   devise_for :users
+   root to: "items#index"
+   resources :items, only: [:new] 
+   get 'items/index'
 end
