79faf62cff1bee5838a4cc293efe55a9cfd3189b
sskyug <sskyugo972@gmail.com>
2022-03-05 18:45:17 +0900

    itemsモデルにバリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ApplicationRecord
 
   belongs_to :user
 
-  validates :items_name, :detail, :category_id, :condition_id, :delivery_charge_id, 
+  validates :image, :items_name, :detail, :category_id, :condition_id, :delivery_charge_id, 
   :prefecture_id, :send_day_id, :price, presence: true
 
   validates :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :send_day_id,
