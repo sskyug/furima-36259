bb947e03bff4cd974d0d3f67c3b23b4d9d0636e6
sskyug <sskyugo972@gmail.com>
2022-03-08 13:56:48 +0900

    itemsコントローラー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,23 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @items = Items.new
+    @item = Item.new
   end
+
+  def create
+    @item = Item.new (item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:items_name,:detail,:category_id,:condition_id,:delivery_charge_id,:prefecture_id,:send_day_id)
+  end
+
 
 end
