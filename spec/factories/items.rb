73dfb98f11397da1f555f4ea04958fcdbb57aaae
sskyug <sskyugo972@gmail.com>
2022-03-11 15:10:30 +0900

    itemモデルのFactoryBotの設定

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    items_name          { Faker::Commerce.product_name }
+    detail              { Faker::Lorem.sentence }
+    category_id           {Faker::Number.between(from: 2, to: 11)}
+    condition_id           {Faker::Number.between(from: 2, to: 7)}
+    delivery_charge_id     {Faker::Number.between(from: 2, to: 3)}
+    prefecture_id          {Faker::Number.between(from: 2, to: 48)}
+    send_day_id            {Faker::Number.between(from: 2, to: 4)}
+    price               {Faker::Number.between(to: 9999999)}
+    association :user
   end
 end
