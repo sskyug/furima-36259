0e1131ed793c3176adb7f7d5953cd967c866e691
sskyug <sskyugo972@gmail.com>
2022-03-11 15:11:06 +0900

    createのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
 
    devise_for :users
    root to: "items#index"
-   resources :items, only: [:new] 
-   get 'items/index'
-end
+   resources :items, only: [:new, :create] 
+  end
