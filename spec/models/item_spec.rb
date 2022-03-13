817d6fa463cdb00d98b90f7fb89510bdd319f3dc
sskyug <sskyugo972@gmail.com>
2022-03-13 23:17:38 +0900

    itemモデルの単体テスト

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,79 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+   @item = FactoryBot.build(:item)
+   @item.image = fixture_file_upload('app/assets/images/star.png')
+  end
+
+  describe '商品出品' do
+    context '問題なく商品が出品できる場合' do
+      it '各項目入力漏れがなければ商品を出品できる' do
+        @user = FactoryBot.build(:user)
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品が出品できない場合' do
+      it '画像がないと出品できない'do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+
+      it '商品名がないと出品できない' do
+        @item.items_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Items name can't be blank")
+      end
+
+      it '商品の説明文がないと出品できない' do
+        @item.detail = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Detail can't be blank")
+      end
+
+      it '商品のカテゴリーが---だと出品できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+
+      it '商品の状態が---だと出品できない' do
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+
+      it '商品の配送料が---だと出品できない' do
+        @item.delivery_charge_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery charge can't be blank")
+      end
+
+      it '商品の発送元地域が---だと出品できない' do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+
+      it '商品の発送までの日数が---だと出品できない' do
+        @item.send_day_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Send day can't be blank")
+      end
+      
+      it '値段を入力しないと出品できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+
+
+    end
+
+  end
+
+
+
 end
