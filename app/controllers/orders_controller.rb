b3a2067ff000e53ea710fc88311080f6f455d9fa
sskyug <sskyugo972@gmail.com>
2022-04-14 02:09:17 +0900

    payjpのコントローラー設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,8 +13,9 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(purchase_address_params)
     if @purchase_address.valid?
+      pay_item
       @purchase_address.save
-      redirect_to root
+      redirect_to root_path
     else
       render :index
     end
@@ -23,6 +24,15 @@ class OrdersController < ApplicationController
   private
 
   def purchase_address_params
-    params.require(:purchase_address).permit(:post_code, :prefecture_id, :municipalities, :address, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:purchase_address).permit(:post_code, :prefecture_id, :municipalities, :address, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]    
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: purchase_address_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 end
