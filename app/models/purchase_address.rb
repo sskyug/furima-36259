6a72b04e9a3c1105b5486ec0f1682fb2f42a75e8
sskyug <sskyugo972@gmail.com>
2022-04-14 02:11:08 +0900

    トークンのバリデーション設定

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,11 +1,11 @@
 class PurchaseAddress
   include ActiveModel::Model
-   attr_accessor :post_code, :prefecture_id, :municipalities, :address, :building_name, :telephone_number, :user_id, :item_id
+   attr_accessor :post_code, :prefecture_id, :municipalities, :address, :building_name, :telephone_number, :user_id, :item_id, :token
    
    with_options presence: true do
     validates :post_code,format: {with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :telephone_number, format: {with: /\A\d{10,11}\z/, message: "can't be blank"}
-    validates :municipalities, :address, :user_id, :item_id
+    validates :municipalities, :address, :user_id, :item_id, :token
    end
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
